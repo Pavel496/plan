f80bbd97a6996e3cecfc18f4f5491c59bcaafd70
butusovpn <butusovpn@mail.ru>
2022-12-19 09:31:39 +0300

    exercises

--- exercises/models.py
+++ exercises/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Exercise(models.Model):
     exercise_name = models.CharField(max_length=50)
-    exercise_description = models.CharField(max_length=50)
+    exercise_description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.exercise_name
